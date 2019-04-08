5b2875d3f8e4285ed0181f539bdb7a09b5e82069
Danilo Poccia <dpoccia@gmail.com>
2019-04-08 15:18:31 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+set -e
+USAGE="Usage: $0 {PACKAGES_BUCKET} {STACK_NAME}"
+PACKAGES_BUCKET=${1?$USAGE}
+STACK_NAME=${2?$USAGE}
 sam build
-sam package --s3-bucket danilop-packages --output-template-file packaged.yaml
-sam deploy --template-file packaged.yaml --stack-name serverless-ml --capabilities CAPABILITY_IAM
+sam package --s3-bucket $PACKAGES_BUCKET --output-template-file packaged.yaml
+sam deploy --template-file packaged.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_IAM
